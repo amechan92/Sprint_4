76169a786084c7aaf8e8827acf568b4ff551dfb2
Elena Aleksandrova <himitsu1892@gmail.com>
2024-03-25 18:23:35 +0500

    азрешение конфликтов слияния

--- src/test/java/tests/MainPageAccordionTest.java
+++ src/test/java/tests/MainPageAccordionTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.JavascriptExecutor;
 import pages.MainPage;
 
 import static org.junit.Assert.assertTrue;
